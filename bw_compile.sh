793d1838fca76c277452a57da9141075b83e5048
Bryan J. Lunt <bjlunt2@illinois.edu>
2021-03-21 01:06:24 -0500

    updated v1.4.0

--- bw_compile.sh
+++ bw_compile.sh
@@ -3,22 +3,39 @@
 #PBS -q high
 #PBS -lnodes=1:ppn=16,walltime=20:00:00
 
+INSTALL_BASEDIR=/projects/eot/bbey/bjlunt2_installs
+INSTALL_CONDA_BASE=/projects/eot/bbey/bjlunt2_installs/miniconda3
+INSTALL_CONDA_ENV_NAME="torch141"
+
+INSTALL_CONDA_ENV_PATH=${INSTALL_CONDA_BASE}/envs/${INSTALL_CONDA_ENV_NAME}
+
+PYTORCH_SOURCE_DIR=/projects/eot/bbey/bjlunt2_installs/src/pytorch
+
+
+#
+#Remember to get cuDNN and NCCL and those will get installed in the environment's include and lib paths.
+#
+
 module load gcc/5.3.0 cmake/3.9.4 cudatoolkit/9.1.85_3.10-1.0502.df1cc54.3.1 cray-libsci/18.12.1 
-source /projects/eot/bbcj/bjlunt2/opt/miniconda3/bin/activate
-source activate torch
+source ${INSTALL_CONDA_BASE}/bin/activate
+source activate ${INSTALL_CONDA_ENV_NAME}
 
 #TODO: compile/tune numpy/scipy to use the cray-libsci implementations of BLAS
 
 #conda install hdf5
 #conda install numpy ninja pyyaml mkl mkl-include setuptools cmake cffi
+#conda install -c pytorch magma-cuda90
 #pip install wandb
 
-cd /projects/eot/bbcj/bjlunt2/src/pytorch
+cd ${PYTORCH_SOURCE_DIR}
 
+#git checkout v1.4.0
 #git submodule sync
 #git submodule update --init --recursive
 
 #TODO: Remember to apply patch
+# git apply ../blue-waters-pytorch/pt-bw.patch
+
 
 #Basic compiler things
 export CC=gcc
@@ -40,14 +57,22 @@ export MKLDNN_THREADING=OMP
 export USE_CUDA=1
 export TORCH_CUDA_ARCH_LIST="3.5" #NVIDIA GK110 (K20X) "Kepler" GPUs on Blue Waters
 
-export CUDNN_LIB_DIR="/projects/eot/bbcj/bjlunt2/src/cuda/lib64"
-export CUDNN_INCLUDE_DIR="/projects/eot/bbcj/bjlunt2/src/cuda/include"
+#export CUDNN_LIB_DIR="/projects/eot/bbcj/bjlunt2/src/cuda/lib64"
+#export CUDNN_INCLUDE_DIR="/projects/eot/bbcj/bjlunt2/src/cuda/include"
+export CUDNN_LIB_DIR=${INSTALL_CONDA_ENV_PATH}/lib
+export CUDNN_INCLUDE_DIR=${INSTALL_CONDA_ENV_PATH}/include
+
 #CUDNN_INCLUDE_PATH
 
 export USE_SYSTEM_NCCL=1
-export NCCL_ROOT="/projects/eot/bbcj/bjlunt2/src/nccl_2.1.15-1+cuda9.1_x86_64"
-export NCCL_LIB_DIR="/projects/eot/bbcj/bjlunt2/src/nccl_2.1.15-1+cuda9.1_x86_64/lib"
-export NCCL_INCLUDE_DIR="/projects/eot/bbcj/bjlunt2/src/nccl_2.1.15-1+cuda9.1_x86_64/include"
+#export NCCL_ROOT="/projects/eot/bbcj/bjlunt2/src/nccl_2.1.15-1+cuda9.1_x86_64"
+#export NCCL_LIB_DIR="/projects/eot/bbcj/bjlunt2/src/nccl_2.1.15-1+cuda9.1_x86_64/lib"
+#export NCCL_INCLUDE_DIR="/projects/eot/bbcj/bjlunt2/src/nccl_2.1.15-1+cuda9.1_x86_64/include"
+
+#I copied the NCCL to here to make things self-contained. The build scripts won't build unless there is a complete NCCL directory. Copying the libs/includes doesn7t work.
+export NCCL_ROOT=${INSTALL_CONDA_ENV_PATH}/share/nccl
+export NCCL_LIB_DIR=${NCCL_ROOT}/lib
+export NCCL_INCLUDE_DIR=${NCCL_ROOT}/include
 
 #
 #Other Options
