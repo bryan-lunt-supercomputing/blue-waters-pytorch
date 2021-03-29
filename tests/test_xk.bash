55dd0d89c9b60fe03fe774fde7f3968938099fda
Bryan Lunt <bjlunt2@illinois.edu>
2021-03-28 23:19:45 -0500

    Added some test scripts.

---
+++ tests/test_xk.bash
@@ -0,0 +1,16 @@
+#!/bin/sh
+#PBS -l nodes=01:ppn=16:xk
+#PBS -l walltime=02:00:00
+#PBS -l gres=shifter
+####PBS -N your_job_name
+####PBS -e $PBS_JOBID.err
+####PBS -o $PBS_JOBID.out
+####PBS -m bea
+##PBS -M YOUR_NETID@illinois.edu
+
+module load shifter
+cd ${PBS_O_WORKDIR}
+
+shifterimg pull luntlab/bw-pytorch:v1.5.0-devel
+
+aprun -b -- shifter --image=docker:luntlab/bw-pytorch:v1.5.0-devel --module=mpich,gpu -- python dist_mpi.py
